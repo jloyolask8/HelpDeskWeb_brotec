97df06e6ca0274e38ff20507acb742a13f95ad59
Jorge Flores <jorge@godesk.cl>
2014-07-30 03:17:43 -0400

    new controller for responsible field

---
+++ src/main/java/com/itcs/helpdesk/jsfcontrollers/ResponsableController.java
@@ -0,0 +1,134 @@
+package com.itcs.helpdesk.jsfcontrollers;
+
+import com.itcs.helpdesk.jsfcontrollers.util.JsfUtil;
+import com.itcs.helpdesk.jsfcontrollers.util.PaginationHelper;
+import com.itcs.helpdesk.persistence.entities.Responsable;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
+import org.primefaces.model.SelectableDataModel;
+
+
+@ManagedBean(name = "responsableController")
+@SessionScoped
+public class ResponsableController extends AbstractManagedBean<Responsable> implements Serializable {
+
+    public ResponsableController() {
+        super(Responsable.class);
+    }
+
+    @Override
+    public PaginationHelper getPagination() {
+        if (pagination == null) {
+            pagination = new PaginationHelper(getPaginationPageSize()) {
+                @Override
+                public int getItemsCount() {
+                    return getJpaController().count(Responsable.class).intValue();
+                }
+
+                @Override
+                public DataModel createPageDataModel() {
+                    return new ResponsableDataModel(getJpaController().queryByRange(Responsable.class, getPageSize(), getPageFirstItem()));
+                }
+            };
+        }
+        return pagination;
+    }
+
+    public DataModel getItems() {
+        if (items == null) {
+            items = getPagination().createPageDataModel();
+        }
+        return items;
+    }
+
+    @Override
+    public Class getDataModelImplementationClass() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public SelectItem[] getItemsAvailableSelectMany() {
+        return JsfUtil.getSelectItems(getJpaController().findAll(Responsable.class), false);
+    }
+
+    @Override
+    public SelectItem[] getItemsAvailableSelectOne() {
+        return JsfUtil.getSelectItems(getJpaController().findAll(Responsable.class), true);
+    }
+
+    @FacesConverter(forClass = Responsable.class)
+    public static class ResponsableControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            ResponsableController controller = (ResponsableController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "responsableController");
+            return controller.getJpaController().find(Responsable.class, getKey(value));
+        }
+
+        java.lang.Integer getKey(String value) {
+            java.lang.Integer key;
+            key = Integer.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Integer value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Responsable) {
+                Responsable o = (Responsable) object;
+                return getStringKey(o.getIdResponsable());
+            } else {
+                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: " + Responsable.class.getName());
+            }
+        }
+    }
+}
+class ResponsableDataModel extends ListDataModel<Responsable> implements SelectableDataModel<Responsable> {
+
+    public ResponsableDataModel() {
+        //nothing
+    }
+
+    public ResponsableDataModel(List<Responsable> data) {
+        super(data);
+    }
+
+    @Override
+    public Responsable getRowData(String rowKey) {
+        List<Responsable> listOfResponsable = (List<Responsable>) getWrappedData();
+
+        for (Responsable obj : listOfResponsable) {
+            if (obj.getIdResponsable() == Integer.parseInt(rowKey)) {
+                return obj;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public Object getRowKey(Responsable classname) {
+        return classname.getIdResponsable();
+    }
+}
