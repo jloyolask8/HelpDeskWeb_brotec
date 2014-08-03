04c801b38782cd75c8d62cde9c4af9be58ea827a
Jorge Flores <jorge@godesk.cl>
2014-08-02 22:10:39 -0400

    Se agrega codigo para decodificar from

--- src/main/java/com/itcs/helpdesk/util/ManagerCasos.java
+++ src/main/java/com/itcs/helpdesk/util/ManagerCasos.java
@@ -56,6 +56,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.mail.internet.MimeUtility;
 import javax.persistence.NoResultException;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.mail.EmailAttachment;
@@ -501,8 +502,6 @@ public class ManagerCasos implements Serializable {
         return createdCaso;
     }
 
-    
-
     public boolean crearCasoDesdeEmail(Canal canal, EmailMessage item) {
         boolean retorno = false;
         try {
@@ -523,16 +522,17 @@ public class ManagerCasos implements Serializable {
                 datos.setEmail(item.getFromEmail().toLowerCase().trim());
 
                 if (item.getFromName() != null) {
-                    if (!item.getFromName().contains("=?ISO") && !item.getFromName().contains("UTF-8")) {//Bug Thunderbird
-                        String[] nombres = item.getFromName().split(" ");
-                        if (nombres.length > 0) {
-                            datos.setNombre(nombres[0]);
-                        }
-                        if (nombres.length > 1) {
-                            datos.setApellidos(nombres[1]);
-                        }
+                    
+                    String from = MimeUtility.decodeText(item.getFromName().replace("\"", ""));
+                    String[] nombres = from.split(" ");
+                    if (nombres.length > 0) {
+                        datos.setNombre(nombres[0]);
                     }
-
+                    if (nombres.length > 2) {
+                        datos.setApellidos(nombres[1] + " " + nombres[2]);
+                    } else if (nombres.length > 1) {
+                        datos.setApellidos(nombres[1]);
+                    }
                 }
 
                 datos.setTipoCaso(EnumTipoCaso.CONTACTO.getTipoCaso().getIdTipoCaso());
