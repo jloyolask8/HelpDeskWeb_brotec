b9636761b0b00b074de578c883c4aa66e34d6dcd
Jorge Flores <jorge@godesk.cl>
2014-12-04 14:56:58 -0300

    Pequeñas mejoras
    - Se agrega la config de ckeditor para subir imagenes
    - Se mejora la visualización de la barra de busqueda
    - Se soluciona el problema que en el entityCriteria con el tema redmond
    usaba dos lineas.

--- src/main/webapp/resources/js/ckeditor.js
+++ src/main/webapp/resources/js/ckeditor.js
@@ -1,5 +1,10 @@
 
-CKEDITOR.editorConfig = function(config) {
- var contextPath = $('.contextPath').html();
- config.filebrowserImageUploadUrl = contextPath+'../../../ckeditor/uploadimage';
+CKEDITOR.editorConfig = function (config) {
+    //var contextPath = '';
+    var contextPath = getContextPath();
+    config.filebrowserImageUploadUrl = contextPath + '/faces/ckeditor/uploadimage';
 };
+
+function getContextPath() {
+   return window.location.pathname.substring(0, window.location.pathname.indexOf("/",2));
+}
